07dd488350fe7cab9076395554e5e9fadac0cab2
rongchang <rongchangxie@douyutech.cn>
2023-05-19 16:47:14 +0800

    no message

---
+++ react18-ts4-webpack5-starter/src/Mybutton.tsx
@@ -0,0 +1,11 @@
+import '@/styles/myButton.scss'
+export default function MyButton() {
+  return (
+    <div className='bg'>
+      <h2>
+        haha
+        <span className='hh'>无语</span>
+      </h2>
+    </div>
+  )
+}
